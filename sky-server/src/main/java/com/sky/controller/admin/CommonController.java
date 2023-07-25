4c1fe23baae7aa692c83a3be57a095377f3d7c23
Lzj <784661716@qq.com>
2023-07-25 10:03:22 +0800

    文件上传功能代码实现

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,96 @@
+package com.sky.controller.admin;
+
+import com.sky.constant.MessageConstant;
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * 通用接口
+ */
+@RestController
+@Slf4j
+@Api(tags = "通用接口")
+@RequestMapping("/admin/common")
+public class CommonController {
+
+    @Value("${sky.path}")
+    private String basePath;
+
+    /**
+     * 文件上传
+     * @param file
+     * @return
+     */
+    @ApiOperation("文件上传")
+    @PostMapping("/upload")
+    public Result<String> upload(MultipartFile file){
+        log.info("文件上传:{}",file);
+        String originalFilename = file.getOriginalFilename();
+        String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+
+        String fileName = UUID.randomUUID().toString() + suffix;
+
+        File dir = new File(basePath);
+        if(!dir.exists()){
+            dir.mkdirs();
+        }
+        try {
+            file.transferTo(new File(basePath+fileName));
+            return Result.success(fileName);
+        } catch (IOException e) {
+            log.error("文件上传失败:{}",e);
+        }
+
+        return Result.error(MessageConstant.UPLOAD_FAILED);
+    }
+
+    /**
+     * 文件下载
+     * @param name
+     * @param response
+     * @return
+     */
+
+    /*
+    @ApiOperation("文件下载")
+    @GetMapping("/download")
+    public Result download(String name, HttpServletResponse response){
+        try {
+            FileInputStream fileInputStream = new FileInputStream(new File(basePath+name));
+            ServletOutputStream outputStream = response.getOutputStream();
+
+            response.setContentType("image/jpeg");
+
+            int len=0;
+            byte[] bytes=new byte[1024];
+
+            while ((fileInputStream.read(bytes))!=-1){
+                outputStream.write(bytes,0,len);
+                outputStream.flush();
+            }
+            outputStream.close();
+            fileInputStream.close();
+            return Result.success();
+        } catch (Exception e) {
+            log.error("文件下载失败:{}",e);
+        }
+        return Result.error("文件下载失败");
+    }
+     */
+}
