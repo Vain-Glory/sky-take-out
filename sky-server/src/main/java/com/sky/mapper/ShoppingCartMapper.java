7fc710f153cb26c7e080fd9439794ef1c33b40f4
Lzj <784661716@qq.com>
2023-07-31 17:02:08 +0800

    查看购物车业务代码开发

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -11,7 +11,7 @@ import java.util.List;
 public interface ShoppingCartMapper {
     /**
      * 动态条件查询
-     * @return
+     * @param shoppingCart
      */
     List<ShoppingCart> list(ShoppingCart shoppingCart);
 
